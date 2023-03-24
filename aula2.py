a19be50674610fdfd939cadca4a6e15607e6d8ab
Guest 02 <guest02@alunos.sc.senac.br>
2023-03-24 11:15:10 -0300

    exercicio final

---
+++ aula2.py
@@ -0,0 +1,35 @@
+# print(2**10)
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print("Ronanderson"*10)
+
+# nome = "Ronanderson"
+# peso = 120
+# altura = 1.85
+# imc = peso/(altura*altura)
+# print("ola", nome, "seu peso é", peso, "kgs e você tem", altura, "metros de altura, consequentemente seu imc é de", imc)
+
+# a = 2
+# b = 1
+# c = -3
+
+# delta = b**2-4*a*c
+# bascara = (-b + (delta**(1/2))) / (2 * a)
+# alvo = ( -b - (delta**(1/2))) / (2 * a)
+
+# print("bascara mais", bascara, "alvo menos", alvo)
+# pi = 3.1415
+# print(f"o valor de bascara é {bascara}")
+# print(f"pi: {pi:.5f}") 
+# # O F anula ponto virgula e aspas desnecessárias 
+
+# print(f"pi: {pi:.2f}")
+
+# nome = "Ronanderson"
+# idade = 25
+# linha = ' {} tem {} anos de idade'.format(nome,idade)
+# print(linha)
+
+numero = float(input("digite suaidade"))
+print(numero)
