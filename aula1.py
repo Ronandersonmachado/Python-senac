50ce6b3eb7e3749979846c682fee9a543bf08932
Guest 02 <guest02@alunos.sc.senac.br>
2023-03-24 09:46:27 -0300

    Primeiro exercicio aula02

---
+++ aula1.py
@@ -0,0 +1,14 @@
+print(2**10)
+print(5/2)
+print(5//2)
+print(5%2)
+print("Ronanderson"*10)
+
+nome = "Ronanderson"
+peso = 120
+altura = 1.85
+imc = peso/(altura*altura)
+print("ola", nome, "seu peso é", peso, "kgs e você tem", altura, "metros de altura, consequentemente seu imc é de", imc)
+
+
+
